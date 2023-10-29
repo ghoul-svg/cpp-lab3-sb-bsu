2575d7b5a4101b19df656b1445bf42e3e3e2ffcb
ghoul-svg <145867597+ghoul-svg@users.noreply.github.com>
2023-10-29 16:54:21 +0300

    Update 3a-t4.cpp
    iww

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,14 +1,24 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": ";
+int k, i = 0, sum_k = 0, number;
+cout << " Enter the amount of integers to sum nr." << i<<": ";
+cin >> k;
+ if (k == 0)
+ {
+cout << "The sum of 0 integers is: 0" << endl;
+return 0;
+}
+do
+{
+cout << "Enter integer: ";
 cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: " << sum_k;
+sum_k += number;
+i++;
+}
+while (i<k);
+cout << " The total sum of " << k << " integers is: " << sum_k << endl;
 return 0;
 }
